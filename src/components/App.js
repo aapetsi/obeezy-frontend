620208b8a66326d86b343e58b8dfee4a537f8b0b
aapetsi <apetsiampiah@gmail.com>
2020-04-23 15:52:48 +0000

    setup redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import AppRouter from '../routers/AppRouter'
+import { Provider } from 'react-redux'
+import store from '../store'
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <AppRouter />
+        <Provider store={store}>
+          <AppRouter />
+        </Provider>
       </div>
     </React.StrictMode>
   )
